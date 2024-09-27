2e7ea199d9c206e2f75b0332b649a9b5f45e16c2
Do Ngoc Minh <minhdn@rikkeisoft.com>
2024-09-27 10:25:48 +0700

    2.1 Add Discussion Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_25_081415) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_27_031939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "name"
+    t.boolean "pinned", default: false
+    t.boolean "close", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_discussions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_25_081415) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "discussions", "users"
 end
